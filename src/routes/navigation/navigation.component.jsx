ec3a1b6d2b9ce94b64b80c2a93277cbc4700ab9c
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2024-03-29 15:52:14 +0500

    netlify deploy fix

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
 import { CartContext } from "../../contexts/cart.context";
@@ -7,7 +7,6 @@ import { CartContext } from "../../contexts/cart.context";
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as OldenLogo } from '../../assets/olden.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import './navigation.styles.scss';
