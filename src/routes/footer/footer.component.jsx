27b11e9a8b479374fd67fb9e016734ee01aa1a16
Mirzokhid Nazirov <mirzokhidnazirov97@gmail.com>
2024-03-30 15:14:41 +0500

    made footer always at the bottom

--- src/routes/footer/footer.component.jsx
+++ src/routes/footer/footer.component.jsx
@@ -1,13 +1,37 @@
-import './footer.styles.scss'
+import { useEffect, useState, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import "./footer.styles.scss";
 
 function Footer() {
-  const currentYear = new Date().getFullYear()
+  const [contentEnough, setContentEnough] = useState(false);
+  const footerRef = useRef();
+  const location = useLocation();
+
+  const currentYear = new Date().getFullYear();
+
+  useEffect(() => {
+    const body = document.body;
+    const html = document.documentElement;
+    const windowHeight = window.innerHeight;
+    const contentHeight = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight
+    );
+
+    setContentEnough(contentHeight > windowHeight);
+  }, [location.pathname]);
 
   return (
-    <div className="footer-container">
+    <div
+      className={`footer-container ${!contentEnough && "lower-footer"}`}
+      ref={footerRef}
+    >
       <span>&#169; The Olden {currentYear}. No Rights Reserved</span>
     </div>
   );
 }
 
-export default Footer
+export default Footer;
